8859cc2db67e75869705f58b1a09000c63317e59
Emil [carco] Sirbu <emil.sirbu@gmail.com>
2013-04-15 08:49:52 +0300

    fix path

--- shell/magendoo/backup.sh
+++ shell/magendoo/backup.sh
@@ -3,10 +3,12 @@
 CWD=$(readlink -f "$0")
 PWD=$(dirname "$CWD")
 
+die () { [ -n "$2" ] && echo "$2" >&2; exit $1; }
+
 MAGENTOROOT=$(readlink -f "${PWD}/../../")
 #Set magento root if "automatic" fail
 #MAGENTOROOT=/home/carco/htdocs/magento/
-
+[ -d "${MAGENTOROOT}" ] || die 100 "Magento root ${MAGENTOROOT}, set manual."
 
 #You can define here connection data (if you don't want to read from local.xml)
 declare -A CONN
@@ -16,19 +18,23 @@ declare -A CONN
 #CONN[password]="password"
 #CONN[table_prefix]=""
 
-LOCALXMLPATH="${MAGENTOROOT}/app/etc/local.xml"
-BACKUPPATH="${MAGENTOROOT}/var/backups"
-mkdir -p "$BACKUPPATH"
 
-SKIPLOGS=1
+
+LOCALXMLPATH="${MAGENTOROOT}/app/etc/local.xml"
+BACKUPPATH="${MAGENTOROOT}/var/backup"
+mkdir -p "$BACKUPPATH" || die 125 "Cannot create backup directory, $BACKUPPATH"
+
+[ -f "${LOCALXMLPATH}" ] || die 150 "local.xml not found, $LOCALXMLPATH"
+[ -d "${BACKUPPATH}" ]   || die 175 "backup directory not found, $BACKUPPATH"
+
 
 DATETIME=`date -u +"%Y%m%d%H%M"`
 DBFILENAME="$DATETIME.sql.gz"
 
+SKIPLOGS=1
+
 #Add here ignored tables
-IGNOREDTABLES="core_cache
-core_cache_option
-core_cache_tag
+IGNOREDTABLES="
 log_customer
 log_quote
 log_summary
@@ -50,7 +56,7 @@ dataflow_batch_import"
 PARAMS="host username dbname password table_prefix"
 for PARAM in $PARAMS; do
     if [ -z "${CONN[$PARAM]}" ]; then
-        CONN[$PARAM]=$(sed -n "/<connection>/,/<\/connection>/p" $LOCALXMLPATH | sed -n -e "s/.*<$PARAM><!\[CDATA\[\(.*\)\]\]><\/$PARAM>.*/\1/p" | head -n 1)
+        CONN[$PARAM]=$(sed -n "/<connection>/,/<\/connection>/p" ${LOCALXMLPATH} | sed -n -e "s/.*<${PARAM}><!\[CDATA\[\(.*\)\]\]><\/${PARAM}>.*/\1/p" | head -n 1)
     fi;
 done
 
@@ -67,7 +73,7 @@ CONNECTIONPARAMS=" -u'${CONN[username]}' -h'${CONN[host]}' -p'${CONN[password]}'
 IGNOREPARAMS=
 if [ -n "$SKIPLOGS" ] ; then
     for TABLENAME in $IGNOREDTABLES; do
-        IGNOREPARAMS="$IGNOREPARAMS --ignore-table='${CONN[dbname]}'.'${CONN[table_prefix]}$TABLENAME'"
+        IGNOREPARAMS="${IGNOREPARAMS} --ignore-table='${CONN[dbname]}'.'${CONN[table_prefix]}${TABLENAME}'"
     done
 fi
 
@@ -75,10 +81,13 @@ if [ -z "$IGNOREPARAMS" ]; then
     CODEDUMPCMD="mysqldump $CONNECTIONPARAMS"
 else
     #FIRST, EXPORT SCHEMA, THEN DATA
-    CODEDUMPCMD="(mysqldump --no-data $CONNECTIONPARAMS; mysqldump --no-create-info $CONNECTIONPARAMS $IGNOREPARAMS )"
+    CODEDUMPCMD="(mysqldump --no-data ${CONNECTIONPARAMS}; mysqldump --no-create-info ${CONNECTIONPARAMS} ${IGNOREPARAMS} )"
 fi
 
 
-CODEDUMPCMD="$CODEDUMPCMD | gzip > $BACKUPPATH/$DBFILENAME"
+CODEDUMPCMD="${CODEDUMPCMD} | gzip > ${BACKUPPATH}/${DBFILENAME}"
 #echo $CODEDUMPCMD
-eval "$CODEDUMPCMD"
+echo -n "Dump ${CONN[dbname]} database to ${BACKUPPATH}/${DBFILENAME}... "
+eval "$CODEDUMPCMD" && echo "DONE" || die 254 "FAIL"
+
+
